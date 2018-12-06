79cb0960492e305d5fb65c7590b6cda9bf5b5958
Sergey Pashnin <s.pashnin@encry.ru>
2018-12-06 15:34:10 +0300

    add итератор

--- 1-lesson/es6/scripts.js
+++ 1-lesson/es6/scripts.js
@@ -1,3 +1,69 @@
+// import counter from './some.js'
+// import * as counter from './some.js'
+import {cnt} from './some.js'
+import {inc} from './some.js'
+
+
 let elem = document.querySelector('.test');
-elem.innerHTML = 'test';
+elem.innerHTML = 'test-page';
 elem.style.cssText = 'border: 1px solid red; text-align: center; font-size: 20px';
+
+console.log(cnt);
+inc();
+inc();
+inc();
+console.log(cnt);
+
+
+/*Symbol*/
+
+let forName1 = Symbol("name");
+let otherName1 = Symbol("name");
+// // Symbol не равен Symbol
+// console.log(forName1 === otherName1);
+
+let pasport = Symbol();
+
+let user = {
+    firstName: 'Name',
+    lastName: 'User',
+    [pasport]: '6543218'
+};
+
+for (let key in user) {
+    console.log(`${key}: ${user[key]}`)
+}
+
+console.log(user[pasport]);
+
+/*Итераторы*/
+
+let someObj = {
+    to: 10,
+    [Symbol.iterator]: function () {
+
+        let current = 0;
+        let stop = this.to;
+
+        return {
+            next() {
+                if (current <= stop) {
+                    return {
+                        done: false,
+                        value: current++
+                    }
+                }
+                else {
+                    return {
+                        done: true
+                    }
+                }
+            }
+        }
+    }
+};
+
+for (let some of someObj) {
+    console.log(some);
+}
+
---
+++ 1-lesson/es6/some.js
@@ -0,0 +1,12 @@
+// export default {
+//     cnt: 0,
+//     inc() {
+//         this.cnt++;
+//     }
+// }
+
+export let cnt = 0;
+
+export function inc() {
+    cnt++;
+}
